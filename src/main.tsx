ad84240708cfd477e6c845135ad32df475b52748
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2023-02-26 23:02:19 +0700

    fix: delete unused dependency

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { Tooltip } from 'react-tooltip';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { HelmetProvider } from 'react-helmet-async';
 import store, { persistor } from '@/store';
 import { Loader } from '@/component/UI';
 import App from './App';
@@ -16,16 +15,14 @@ import 'react-loading-skeleton/dist/skeleton.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Provider store={store}>
-        <PersistGate loading={<Loader />} persistor={persistor}>
-          <Router>
-            <App />
-          </Router>
-        </PersistGate>
-        <Toaster />
-        <Tooltip id="tooltip" className="!bg-primary" />
-      </Provider>
-    </HelmetProvider>
+    <Provider store={store}>
+      <PersistGate loading={<Loader />} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
+      <Toaster />
+      <Tooltip id="tooltip" className="!bg-primary" />
+    </Provider>
   </React.StrictMode>
 );
